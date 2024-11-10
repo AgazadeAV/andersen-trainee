e5bb342413558643db34efd3a3f154df0f7a0309
Azer Agazade <azer.agazade05@gmail.com>
2024-11-10 21:54:17 +0400

    added config.properties as a constant

--- hotel/src/main/java/com/andersenhotels/model/config/ConfigManager.java
+++ hotel/src/main/java/com/andersenhotels/model/config/ConfigManager.java
@@ -7,19 +7,20 @@ import java.util.Properties;
 
 public class ConfigManager {
 
+    private static final String CONFIG_FILE = "config.properties";
     private static final Properties properties = new Properties();
 
     static {
-        try (InputStream input = ConfigManager.class.getClassLoader().getResourceAsStream("config.properties")) {
+        try (InputStream input = ConfigManager.class.getClassLoader().getResourceAsStream(CONFIG_FILE)) {
             if (input == null) {
-                System.err.println("Configuration file 'config.properties' not found in 'src/main/resources'. "
+                System.err.println("Configuration file '" + CONFIG_FILE + "' not found in 'src/main/resources'. "
                         + "Please ensure the file exists and is placed correctly.");
             } else {
                 properties.load(input);
                 System.out.println("Configuration loaded successfully.");
             }
         } catch (IOException e) {
-            System.err.println("Error occurred while loading configuration file 'config.properties'.");
+            System.err.println("Error occurred while loading configuration file '" + CONFIG_FILE + "'.");
         }
     }
 

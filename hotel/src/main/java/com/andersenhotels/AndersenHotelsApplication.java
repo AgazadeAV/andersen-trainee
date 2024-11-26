9c1946f8bf9b1218f3089fd8744c05155c7a9e4a
azer <azer.agazade05@gmail.com>
2024-11-26 19:59:25 +0400

    added dependencies and renamed main class

--- hotel/src/main/java/com/andersenhotels/AndersenHotelsApplication.java
+++ hotel/src/main/java/com/andersenhotels/AndersenHotelsApplication.java
@@ -4,7 +4,7 @@ import com.andersenhotels.model.storage.LiquibaseRunner;
 import com.andersenhotels.view.View;
 import com.andersenhotels.view.console_ui.ConsoleUI;
 
-public class Main {
+public class AndersenHotelsApplication {
     public static void main(String[] args) {
         LiquibaseRunner.runLiquibaseMigrations();
 
